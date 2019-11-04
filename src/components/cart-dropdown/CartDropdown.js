b57ddc1aa5215f4d99cc463ff7ad14fa5705f374
Ana Nikolova <ani.nik19@gmail.com>
2019-11-04 09:07:32 +0100

    created CartItem Component

--- src/components/cart-dropdown/CartDropdown.js
+++ src/components/cart-dropdown/CartDropdown.js
@@ -1,16 +1,26 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import CustomButton from "../custom-button/CustomButton";
+import CartItem from "../cart-item/CartItem";
 
 import "./cart-dropdown.scss";
 
-const CartDropdown = () => {
+const CartDropdown = ({ cartItem }) => {
   return (
     <div className="cart-dropdown">
-      <div className="cart-items" />
+      <div className="cart-items">
+        {cartItem.map(cartItem => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
+      </div>
       <CustomButton>GO TO CHECKOUT </CustomButton>
     </div>
   );
 };
 
-export default CartDropdown;
+const mapStateToProps = ({ cart: { cartItem } }) => ({
+  cartItem
+});
+
+export default connect(mapStateToProps)(CartDropdown);
